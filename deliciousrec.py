ce18c909e2f9be0acec63df32193d61af7161ec7
Benedict Chen <benedict@benedictchen.com>
2013-12-29 00:59:35 -0800

    Added fill_items to delicious recommender.

--- deliciousrec.py
+++ deliciousrec.py
@@ -24,5 +24,28 @@ def initialize_user_dict(tag, count=5):
             user_dict[user] = {}
     return user_dict
 
+def fill_items(user_dict):
+    all_items = {}
+    # Find links posted by all users.
+    for user in user_dict:
+        for i in range(3):
+            try:
+                posts = pydelicious.get_userposts(user)
+                print posts
+                break
+            except:
+                print "Failed user " + user + ", retrying."
+                time.sleep(4)
+        for post in posts:
+            url = post['url']
+            user_dict[user][url] = 1.0
+            all_items[url] = 1
+
+        # Fill in the missing items with 0.
+        for ratings in user_dict.values():
+            for item in all_items:
+                if item not in ratings:
+                    ratings[item] = 0.0
+
 
 
